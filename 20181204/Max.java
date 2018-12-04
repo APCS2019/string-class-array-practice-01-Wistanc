cf60faea2b7463471e3b7576bfccab5a503670e9
Wistanc <45375832+Wistanc@users.noreply.github.com>
2018-12-04 09:49:20 +0800

    Create Max.java

---
+++ 20181204/Max.java
@@ -0,0 +1,21 @@
+//past paper 2015
+//Main:String and class
+
+public class HiddenWord{
+  private String puzzle;
+}
+public HideenWord(String puzzle){
+  this.puzzle=puzzle;
+}
+public String getHint(String guess){
+  String temp=" ";
+  For(int i=0;i<guess.length();i++){
+     if(puzzle.substring(i,i+1).equals(guess.substring(i,i+1)){
+       temp+=substring(i+1);
+     else if(puzzle.indexOf(guess.substring(i,i+1)){
+       temp+="+";
+     else 
+     temp+="*";
+  }
+ return temp
+}
